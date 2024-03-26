c19b60470bc72f2a75219fbed2ad8eb71d7a0653
javie <alubla1033@ieselcaminas.org>
2024-03-26 16:51:11 +0100

    Structure(26/02/2024)

---
+++ src/main/java/com/example/tfg_rest/TfgRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.tfg_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TfgRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tfg_rest/TfgRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tfg_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
