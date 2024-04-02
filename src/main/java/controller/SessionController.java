562ffe1673c13c196be9388ba9c384d61b852dc4
javier <alubla1033@ieselcaminas.org>
2024-04-02 16:11:45 +0200

    Merge to develop(02/04/2024)

--- src/main/java/controller/SessionController.java
+++ src/main/java/controller/SessionController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import security.JWTAuthenticationConfig;
 
 import java.util.Map;
 
@@ -28,7 +29,7 @@ public class SessionController {
     private PasswordEncoder passwordEncoder;
 
     @Autowired
-    private JWTAuthtenticationConfig authtenticationConfig;
+    private JWTAuthenticationConfig authtenticationConfig;
 
     @PostMapping("/login")
     @PreAuthorize("isAnonimous()")
